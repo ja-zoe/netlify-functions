c67e22031557429bbdfb8d86ee402feefc6e46dc
ja-zoe <j.vilfort01@gmail.com>
2024-07-02 20:53:32 -0400

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 const express = require('express')
 const serverless = require("serverless-http")
+const cors = require('cors')
 
 const PORT = 8025
 const app = express()
 
 const router = express.Router()
+
+app.use(cors())
 
 router.get('/', (req, res) => {
     res.send('This is the root endpoint')
